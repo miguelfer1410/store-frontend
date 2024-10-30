9834233cc0f9d47bf1d6b4cbd0eb62eebf410ff3
Miguel Fernandes <miguelfernandes1410@gmail.com>
2024-10-30 15:56:43 +0000

    store-frontend

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -4,16 +4,6 @@ import { getFirestore } from "firebase/firestore"; // Importa a função getFire
 
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAFrDxhyjuvbQ824uN9DrZMHyOqrp1EhzA",
-  authDomain: "store-c2ad1.firebaseapp.com",
-  projectId: "store-c2ad1",
-  storageBucket: "store-c2ad1.appspot.com",
-  messagingSenderId: "119102546211",
-  appId: "1:119102546211:web:941c80cdbe13542b162c96",
-  measurementId: "G-VEKNYT2GKE"
-};
-
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
